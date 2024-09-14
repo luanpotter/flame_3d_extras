3b7ec57927a2ba6dc07019ed699afdbab6e36957
Luan Nico <luanpotter27@gmail.com>
2024-09-14 14:43:51 -0400

    Expose more parameters in the ModelComponent constructor

--- lib/model/model_component.dart
+++ lib/model/model_component.dart
@@ -14,6 +14,8 @@ class ModelComponent extends Object3D {
   ModelComponent({
     required this.model,
     super.position,
+    super.rotation,
+    super.scale,
   });
 
   Aabb3 get aabb => _aabb
